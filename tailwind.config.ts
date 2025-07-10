43bc01d81d1ba80f9a10f4de4cc1d4b04766afe3
Nandika Mishra <nandika6804@gmail.com>
2025-07-10 01:48:28 +0000

    make the homepage title Playfair Display font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Literata', 'serif'],
-        headline: ['"PT Sans"', 'sans-serif'],
+        headline: ['"Playfair Display"', 'serif'],
         code: ['monospace'],
       },
       colors: {
